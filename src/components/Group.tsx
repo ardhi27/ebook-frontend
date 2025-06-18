0bcb1999901e0067c31f447f186c71bd682b0caf
kmshyn76 <147911990+Ardhi27@users.noreply.github.com>
2025-06-18 17:36:39 +0800

    feat: implement tailwind merge in Group's component

--- src/components/Group.tsx
+++ src/components/Group.tsx
@@ -1,12 +1,21 @@
 import React from "react";
+import { twMerge } from "tailwind-merge";
 interface GroupProps {
   className?: string;
   children: React.ReactNode;
 }
 
 const Group: React.FC<GroupProps> = ({ className, children }) => {
-  const defaultClass = "flex";
-  return <div className={`${defaultClass} ${className}`}>{children}</div>;
+  return (
+    <div
+      className={twMerge(
+        `flex flex-row gap-3 items-center flex-wrap`,
+        className
+      )}
+    >
+      {children}
+    </div>
+  );
 };
 
 export default Group;
