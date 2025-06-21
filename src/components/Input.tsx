d0ca7ee95c92897831295b2aa530de9f183cd427
kmshyn76 <147911990+Ardhi27@users.noreply.github.com>
2025-06-21 19:47:16 +0800

    feat: add input type

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -2,10 +2,14 @@ import { HTMLAttributes, InputHTMLAttributes } from "react";
 
 type InputProps = InputHTMLAttributes<HTMLInputElement>;
 
-const Input = ({ placeholder, ...props }: InputProps) => {
+const inputType = {
+  password: "password",
+  text: "text",
+};
+const Input = ({ type = "text", placeholder, ...props }: InputProps) => {
   return (
     <input
-      type="text"
+      type={type}
       className="w-full bg-black border border-white rounded-lg text-white px-2 py-3 focus:border-teal focus:ring-0"
       placeholder={placeholder}
       {...props}
