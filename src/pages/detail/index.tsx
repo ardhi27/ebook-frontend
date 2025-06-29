021a08e40216b962474fcf66248f3c3b932dc208
kmshyn76 <147911990+Ardhi27@users.noreply.github.com>
2025-06-29 16:42:58 +0800

    feat: add new component to show detail item

---
+++ src/pages/detail/index.tsx
@@ -0,0 +1,9 @@
+import { useEffect, useState } from "react";
+import Group from "../../components/Group";
+import Header from "../../components/Header";
+import Stack from "../../components/Stack";
+import axios from "axios";
+import Card from "../../components/Card";
+import Grid from "../../components/Grid";
+
+const BookDetail = () => {};
